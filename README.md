0f089982526d885f56020abc30f74889a737e88d
guodapang820 <760552856@qq.com>
2026-06-04 13:02:17 +0800

    feat:增加输入功能，更新README

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ ex01:
 
 HelloWorld程序
 
+——v1.0 输出Hello World。
 
-
-
+——v2.0增加键盘输入，输出键盘输入内容功能。
 
--- ex01/HelloWorld.cpp
+++ ex01/HelloWorld.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+	char thing[100];
 	printf("Hello World\n");
+	printf("«Î ‰»Î£∫\n");
+	scanf("%s",thing);
+	printf("%s\n",thing);
 	return 0;
  } 
